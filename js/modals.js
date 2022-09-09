4c452a9375703759091c19b7c9f34dbfa5bbfb1b
cakfer <112299172+cakfer@users.noreply.github.com>
2022-09-09 19:06:58 +0700

    Update modals.js

--- js/modals.js
+++ js/modals.js
@@ -1,7 +1,7 @@
 (function($) {
             "use strict";
                 $(document).ready(function() {
-                        $('.btn-primary').on('click', function() {
+                        $('.button-3').on('click', function() {
                         $('body').addClass("modal-open");
                     });
                     $('.close-modal').on('click', function() {
