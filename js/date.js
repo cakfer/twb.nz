473be8d0265bac2b8996e30aff88ee094a823123
cakfer <112299172+cakfer@users.noreply.github.com>
2022-09-09 20:28:44 +0700

    Create date.js

---
+++ js/date.js
@@ -0,0 +1,6 @@
+//Page Last Modified Date script- By JavaScriptKit.com
+//Visit http://www.javascriptkit.com for full source code
+//This notice must stay intact for use
+
+var docmodified='<!--#echo var="LAST_MODIFIED" -->'
+document.write('This page was last modified: '+docmodified)
