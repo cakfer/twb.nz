758396783d23a47768332f5da7bfd238f42bf5d3
cakfer <112299172+cakfer@users.noreply.github.com>
2022-09-09 20:37:30 +0700

    Update date.js

--- js/date.js
+++ js/date.js
@@ -1,8 +1,4 @@
-<!--#config timefmt="%B %e, %Y"-->
-
-//Page Last Modified Date script- By JavaScriptKit.com
-//Visit http://www.javascriptkit.com for full source code
-//This notice must stay intact for use
-
-var docmodified='<!--#echo var="LAST_MODIFIED" -->'
-document.write('This page was last modified: '+docmodified)
+if (Date.parse(document.lastModified) != 0)
+    document.write('<i>Last modified: '
+                   + document.lastModified
+                   + '</i></small>');
